345cd7a8bfb761cea484db843c7a4a07aeffe065
HarshadaBhide <harshada.bhide@gmail.com>
2018-08-30 23:31:12 -0800

    remove old structure

--- index.js
+++
@@ -1,7 +0,0 @@
-/** @format */
-
-import {AppRegistry} from 'react-native';
-import ICSApp from './src/ICSApp';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => ICSApp);
