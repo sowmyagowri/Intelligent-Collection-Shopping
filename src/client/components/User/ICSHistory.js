dd342f2bb7ef7a76dab60dfc361fec5266a532b2
Harshada Bhide-Apte <HarshadaBhide@users.noreply.github.com>
2018-11-15 13:34:48 -0800

    Add files via upload

--- src/client/components/User/ICSHistory.js
+++ src/client/components/User/ICSHistory.js
@@ -12,7 +12,16 @@ export default class ICSHistory extends React.Component {
   }
 
   componentDidMount(){
-    return fetch('http://192.168.1.128:5000/users1/allRegistered')
+    return fetch('http://192.168.229.1:5000/posts/getById', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          userId: 'test1',
+        }),
+      })
       .then((response) => response.json())
       .then((responseJson) => {
         console.log(responseJson);
@@ -49,7 +58,7 @@ render(){
           data={this.state.dataSource}
           renderItem={({item}) =>
           <View style={ICSStyles.flatview}>
-            <Text style={ICSStyles.profileInfo}>{item.firstName}{item.lastName}</Text>
+            <Text style={ICSStyles.profileInfo}>{item.name}{"   "}{item.category}{"   "}{item.quantity}{"   "}{item.price}</Text>
           </View>
           }
           keyExtractor={(item, index) => item.userId}
